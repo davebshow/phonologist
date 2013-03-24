d4549c15f214461cc573036a50feb602cc65c287
dbshow <dbshow1@gmail.com>
2013-03-24 01:35:37 -0400

    added symbol level methods/started main

--- phonologist/phonologist.py
+++ phonologist/phonologist.py
@@ -121,7 +121,7 @@ class Phonologist( object ):
 		print stoken
 		if len(stoken) > 1:
 			for ndx, syll in enumerate( stoken ):
-				if not self.stressed(syll):
+				if not self._stressed(syll):
 					for sym in syll:
 						if sym in target:
 							try:
@@ -138,19 +138,11 @@ class Phonologist( object ):
 		else:
 			return count_dict
 
-	def stressed( self, token ):
+	def _stressed( self, token ):
 		if STRESS in token:
 			return True
 		else:
 			return False
-
-
-
-
-
-
-
-
 
 
 #########################################################################
@@ -293,34 +285,6 @@ class TokenIterator( object ):
 			raise StopIteration
 
 
-class UnicodeWriter:
-    """
-    A CSV writer which will write rows to CSV file "f",
-    which is encoded in the given encoding.
-    """
-
-    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
-        # Redirect output to a queue
-        self.queue = cStringIO.StringIO()
-        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
-        self.stream = f
-        self.encoder = codecs.getincrementalencoder(encoding)()
-
-    def writerow(self, row):
-        self.writer.writerow([s.encode("utf-8") for s in row])
-        # Fetch UTF-8 output from the queue ...
-        data = self.queue.getvalue()
-        data = data.decode("utf-8")
-        # ... and reencode it into the target encoding
-        data = self.encoder.encode(data)
-        # write to the target stream
-        self.stream.write(data)
-        # empty queue
-        self.queue.truncate(0)
-
-    def writerows(self, rows):
-        for row in rows:
-            self.writerow(row)
 
 
 
