b2e392805995443b1cafb94744e9619a97b23aa3
Javier de la Rosa <versae@gmail.com>
2013-04-02 15:46:32 -0400

    Added another dummy test example

--- phonologist/tests.py
+++ phonologist/tests.py
@@ -4,6 +4,22 @@ import unittest
 from phonologist import Words
 
 
+class Phonologist(object):
+    """docstring for phonologist"""
+
+    def __init__(self):
+        super(Phonologist, self).__init__()
+        self._words = []
+
+    def _get_words(self):
+        return self._words
+
+    def _set_words(self, words_list):
+        self._words = words_list
+
+    words = property(_get_words, _set_words)
+
+
 class PhonologistTestCase(unittest.TestCase):
 
     def setUp(self):
@@ -16,6 +32,11 @@ class PhonologistTestCase(unittest.TestCase):
         w = u"yo escribo un test"
         words = Words(w)
         self.assertEquals(words.tokens, [u'yo', u'escribo', u'un', u'test'])
+
+    def test_one(self):
+        ph = Phonologist()
+        ph.words = ["one", "word"]
+        self.assertEquals(ph.words, ["one", "word"])
 
 
 if __name__ == '__main__':
